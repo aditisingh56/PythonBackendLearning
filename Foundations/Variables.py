e3d5d7680a2f15bc11e88534361d052026284c1d
Aditi Singh <aditisingh14006@gmail.com>
2026-03-01 15:18:02 +0530

    Variables

---
+++ Foundations/Variables.py
@@ -0,0 +1,8 @@
+print("Aditi is a girl")
+print("Aditi is 18")
+print("Aditi is from India")
+# now, we usefd Aditi again and again instead of this we can use a variable to store the value of Aditi and then we can use that variable instead of Aditi again and again
+name = "Aditi"
+age = 18
+country = "India"
+print(f"{name} is {age} years old and is from {country}.")
