dab69f39b2dab1eea44c135ba20b50115a1441f6
Aditi Singh <aditisingh14006@gmail.com>
2026-03-03 23:59:32 +0530

    Simple login logout system

---
+++ Foundations/new.py
@@ -0,0 +1,8 @@
+print("Create your account:")
+
+username = input("Input username: ")
+password = input("Input password: ")
+
+print("Account created!")
+print("Username:", username)
+print("Password:", password)
