ad19f7a5760a61c001c3f86ca18a0b6d8fde34cc
Aditi Singh <aditisingh14006@gmail.com>
2026-03-06 14:20:36 +0530

    Sending dynamic values from through views to the browser

--- django_projects/mysite/myappicreated/views.py
+++ django_projects/mysite/myappicreated/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 # Create your views here.
 
 
@@ -8,4 +8,10 @@ from django.http import HttpResponse
 #     return HttpResponse("<h1>Hello, welcome to my website!</h1>")  # this will return a simple response when we go to the root page of the website. we can also return a template instead of a simple response by using render function and passing the request and the template name as arguments.
 
 def index(request):
-    return render(request, 'index.html') # here we are rendering the index.html template when we go to the root page of the website. we need to create a templates folder in the base directory of the project and then create an index.html file inside that folder and write some html code in that file to see the response when we go to the root page of the website.
+    context = {
+        "name": "Ayushi Singh",
+        "age" :23,
+        "country" : "India"
+    }
+    
+    return render(request, 'index.html' , context) # here we are rendering the index.html template when we go to the root page of the website. we need to create a templates folder in the base directory of the project and then create an index.html file inside that folder and write some html code in that file to see the response when we go to the root page of the website.
