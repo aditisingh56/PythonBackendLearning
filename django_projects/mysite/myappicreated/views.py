dd3bf526c8c736e4e50571e77e9826df10b296d5
Aditi Singh <aditisingh14006@gmail.com>
2026-03-08 01:13:37 +0530

    Made simple word counter

--- django_projects/mysite/myappicreated/views.py
+++ django_projects/mysite/myappicreated/views.py
@@ -7,11 +7,15 @@ from django.shortcuts import render
 # def index(request):  # here, we have written request because every view receives a request, and his request is the input like parameters
 #     return HttpResponse("<h1>Hello, welcome to my website!</h1>")  # this will return a simple response when we go to the root page of the website. we can also return a template instead of a simple response by using render function and passing the request and the template name as arguments.
 
+
+  #this was thw second thing i did
 def index(request):
-    context = {
-        "name": "Ayushi Singh",
-        "age" :23,
-        "country" : "India"
-    }
+   
     
-    return render(request, 'index.html' , context) # here we are rendering the index.html template when we go to the root page of the website. we need to create a templates folder in the base directory of the project and then create an index.html file inside that folder and write some html code in that file to see the response when we go to the root page of the website.
+    return render(request, 'index.html') # here we are rendering the index.html template when we go to the root page of the website. we need to create a templates folder in the base directory of the project and then create an index.html file inside that folder and write some html code in that file to see the response when we go to the root page of the website.
+
+def counters(request):
+    text = request.POST['text'] 
+    amount_words = len(text.split()) # this will split the text into a list of words
+     # Get the text from the query parameters
+    return render(request , "counter.html" , {"amount": amount_words})  # here we are rendering the counter.html template when we go to the root page of the website. we need to create a templates folder in the base directory of the project and then create a counter.html file inside that folder and write some html code in that file to see the response when we go to the root page of the website. we are also passing a context dictionary
